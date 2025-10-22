d793c91e173b7084e453737affcba3196fc0424b
profnando <nando9b7y@gmail.com>
2025-10-22 11:08:41 -0300

    Update bd.js

--- bd.js
+++ bd.js
@@ -4,49 +4,11 @@ const ano1 = [
         titulo: "Introdução a embriologia e espermatogênese",
         descricao: "Exclusivo para enfermagem e sim é o mesmo slide do 2° ano",
         link: "https://www.canva.com/design/DAGvTcSKsA8/AoNxEa02SbzOw4elKRjvNA/view"
-    },
-    {
-        img: "Transporte através da membrana plasmática.png",
-        titulo: "Transporte através da membrana plasmática",
-        descricao: "Introdução e difusão",
-        link: "https://www.canva.com/design/DAGu2W0pXPM/8ETNSU4QzEOos7EpL1aj7g/view?utm_content=DAGu2W0pXPM&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hf80e6e8080"
-    },
-    {
-        img: "osmose.png",
-        titulo: "Transporte através da membrana plasmática",
-        descricao: "Osmose",
-        link: "https://www.canva.com/design/DAGvSodf-qc/E-ewlgJ64OIN0p0-or_93Q/view?utm_content=DAGvSodf-qc&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=haef20ef6bf"
-    },
-    {
-        img: "transporteAtivo.png",
-        titulo: "Transporte através da membrana plasmática",
-        descricao: "Transporte Ativo",
-        link: "https://www.canva.com/design/DAGvxYCQ4M8/iMZl0ydsrDgb3TqzANcKzQ/view?utm_content=DAGvxYCQ4M8&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h4bee63059d"
-    },{
-        img: "cell.png",
-        titulo: "Atividade",
-        descricao: "Transporte via membrana plasmática",
-        link: "https://docs.google.com/document/d/1HYAo1GpwTgxnOZtSv8SgwRuw-S06JF4_fLC3nCtPDWc/edit?usp=sharing"
-    },{
-        img: "Citoplasma.png",
-        titulo: "Citoplasma",
-        descricao: "Transporte via membrana plasmática",
-        link: "https://www.canva.com/design/DAGwm0wWfYU/f9_K4KNWlMH9f7ptq1bITg/view?utm_content=DAGwm0wWfYU&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h2051bca779"
     },{
         img: "ovulogênese.png",
         titulo: "Ovogênese e foliculogênese",
         descricao: "Exclusivo para enfermagem e sim é o mesmo slide do 2° ano",
         link: "https://www.canva.com/design/DAGxvewuVcQ/fQm7OX_8gq-5u9ytKvp8pw/view?utm_content=DAGxvewuVcQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h694cd967b3"
-    },{
-        img: "mitocondria.png",
-        titulo: "Td - revisão",
-        descricao: "organelas citoplasmáticas",
-        link: "https://docs.google.com/document/d/1_H_qGEeAeizChtPOMlIKcHQlnfwtXe2g0evm9ruta9I/edit?usp=sharing"
-    },{
-        img: "embrio1.jpeg",
-        titulo: "Atividade embriologia - Apenas para enfermagem",
-        descricao: "Essa atividade é do primeiro ano, se você é do segundo, saia daqui!",
-        link: "https://docs.google.com/document/d/14XuaUQLEESHxJGgDvFW2sggdkyzQJdZG/edit?usp=sharing&ouid=113421151775825196689&rtpof=true&sd=true"
     },{
         img: "nucleo.png",
         titulo: "Núcleo celular",
@@ -61,77 +23,45 @@ const ano2 = [
         titulo: "Introdução a embriologia e espermatogênese",
         descricao: "Exclusivo para enfermagem e sim é o mesmo slide do 1° ano",
         link: "https://www.canva.com/design/DAGvTcSKsA8/AoNxEa02SbzOw4elKRjvNA/view"
-    },
-    {
-        img: "dentes.jpg",
-        titulo: "Td - Sistema digestivo",
-        descricao: "Faça no caderno",
-        link: "https://docs.google.com/document/d/1gaF42VMtiYlzzy765ECLvYsCgUHRhJZbjo14hDfCovk/edit?usp=sharing"
-    },{
-        img: "Sistema respiratório.png",
-        titulo: "Sistema respiratório",
-        descricao: "",
-        link: "https://www.canva.com/design/DAGwEXAsLSI/gn0NU3XvpTaFW-PhzLjBkg/view?utm_content=DAGwEXAsLSI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h87a816b150"
     },{
         img: "ovulogênese.png",
         titulo: "Ovogênese e foliculogênese",
         descricao: "Exclusivo para enfermagem e sim é o mesmo slide do 1° ano",
         link: "https://www.canva.com/design/DAGxvewuVcQ/fQm7OX_8gq-5u9ytKvp8pw/view?utm_content=DAGxvewuVcQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h694cd967b3"
       },{
-        img: "pulmao.jpg",
-        titulo: "TD - sistema respiratório",
-        descricao: "Revisão para a parcial",
-        link: "https://docs.google.com/document/d/1TdmJ2NBSK0EodVVuhjkIpIqPLZ_hDXzO/edit?usp=sharing&ouid=113421151775825196689&rtpof=true&sd=true"
-      },{
-        img: "embrio2.jpeg",
-        titulo: "Atividade embriologia - Apenas para enfermagem",
-        descricao: "Essa atividade é do segundo ano, se você é do primeiro, saia daqui!",
-        link: "https://docs.google.com/document/d/14XuaUQLEESHxJGgDvFW2sggdkyzQJdZG/edit?usp=sharing&ouid=113421151775825196689&rtpof=true&sd=true"
-    },{
-        img: "Sistema circulatório.png",
-        titulo: "Sistema circulatório",
-        descricao: "Slides com atividades de revisão",
-        link: "https://www.canva.com/design/DAFCT3OJN-Q/4eKALciOIcYTCTRnFQ9g1A/view?utm_content=DAFCT3OJN-Q&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0c69e1be07"
+        img: "Fecundação e desenvolvimento embrionário.png",
+        titulo: "Fecundação e desenvolvimento embrionário",
+        descricao: "Conteúdo exclusivo para a turma de enfermagem",
+        link: "https://www.canva.com/design/DAG1CJgFXO8/6NCg4EySU7UKruoIUohjlA/view?utm_content=DAG1CJgFXO8&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=he6e2d361ab"
     },{
         img: "Sistema endócrino.png",
         titulo: "Sistema endócrino",
         descricao: "",
         link: "https://www.canva.com/design/DAG1MtHMHqs/pUyZXffJ-XcBxIV0Wse_lQ/view?utm_content=DAG1MtHMHqs&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h354e86f27d"
-    },{
-        img: "Fecundação e desenvolvimento embrionário.png",
-        titulo: "Fecundação e desenvolvimento embrionário",
-        descricao: "Conteúdo exclusivo para a turma de enfermagem",
-        link: "https://www.canva.com/design/DAG1CJgFXO8/6NCg4EySU7UKruoIUohjlA/view?utm_content=DAG1CJgFXO8&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=he6e2d361ab"
     }
 ]
 
 const ano3 = [
-    {
-        img: "revisao.webp",
-        titulo: "Questões sobre ácidos nucleicos (sem cálculos)",
-        descricao: "",
-        link: "https://docs.google.com/document/d/1rb-pP7B0keo_UMAaJ7PLGecG5UFDAexP9C67zmE38ho/edit?usp=sharing"
-    },
-    {
-        img: "DNA.jpg",
-        titulo: "Questões sobre ácidos nucleicos envolvendo cálculos",
-        descricao: "",
-        link: "https://www.canva.com/design/DAGwAPuYaZQ/1aXhgTYEZhD48nJfF1Mv-Q/view?utm_content=DAGwAPuYaZQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h70bad97207"
-},
-{
-        img: "protein.jpg",
-        titulo: "Slides síntese proteíca",
-        descricao: "",
-        link:"https://www.canva.com/design/DAGxGDCLKiw/RRbB6DvWtIGK_sVLI6D_dA/view?utm_content=DAGxGDCLKiw&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h7981248c8a"
-},{
-        img: "caderno.jpg",
-        titulo: "Atividade síntese proteíca",
-        descricao: "Faça no caderno, vale ponto de atividades e serve como revisão para a prova",
-        link: "https://docs.google.com/document/d/17l_htInwKOQgYPFfDZz-sSAIcwa7wTtYrO5FYcne4A8/edit?usp=sharing"
-},{
+   {
         img: "genetica.png",
         titulo: "Genética",
         descricao: "Termos e questões",
         link: "https://www.canva.com/design/DAGzEKkJFiI/v3A5R6NGUijOCUtC712Hhg/view?utm_content=DAGzEKkJFiI&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h1aabdca943"
-}
+    },
+     {
+        img: "Heredogramas.png",
+        titulo: "Heredogramas",
+        descricao: "Créditos: Vládia Pereira",
+        link: "https://www.canva.com/design/DAG1A4CA_LY/Oa5LzZdqv1MqWDPOCjnOUg/edit?utm_content=DAG1A4CA_LY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+    },{
+        img: "Dominância incompleta e cruzamento teste.png",
+        titulo: "Dominância incompleta e cruzamento teste",
+        descricao: "Créditos: Vládia Pereira",
+        link: "https://www.canva.com/design/DAG1QLoOy8c/LRtpQuLu6DIcNbzOXI4prQ/edit?utm_content=DAG1QLoOy8c&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+    },{
+        img: "Padrões de herança não mendelianos.png",
+        titulo: "Padrões de herança não mendelianos",
+        descricao: "Créditos: Vládia Pereira",
+        link: "ttps://www.canva.com/design/DAG16a_pRXs/KmTvp-jkchTMDpso577GQQ/edit?utm_content=DAG16a_pRXs&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+    },
 ]
