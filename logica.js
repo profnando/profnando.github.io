a0860e4752a7acbc29fe03b63c2dd89fd3fb00e5
profnando <nando9b7y@gmail.com>
2026-01-20 18:42:56 -0300

    v 200126

--- logica.js
+++ logica.js
@@ -1,3 +1,12 @@
+let bancoSlides = [];
+
+fetch("bd.json")
+  .then(res => res.json())
+  .then(dados => {
+    bancoSlides = dados;
+    carregarPorTag("introducao a biologia", "lista-bio-geral");
+  });
+
 function criarCard(imgNome, titulo, descricao, link, idconteiner) {
   const col = document.createElement("div");
   col.className = "col-md-4 col-sm-6 mt-3 mb-3 g-3 cartao";
@@ -13,6 +22,24 @@ function criarCard(imgNome, titulo, descricao, link, idconteiner) {
     </div>
   `;
 
-  // Adiciona ao container
   document.getElementById(idconteiner).appendChild(col);
+}
+
+function carregarPorTag(tag, idContainer) {
+  const container = document.getElementById(idContainer);
+  container.innerHTML = "";
+
+  const filtrados = bancoSlides.filter(slide =>
+    slide.tags.includes(tag)
+  );
+
+  filtrados.forEach(slide => {
+    criarCard(
+      slide.img,
+      slide.titulo,
+      slide.descricao || "",
+      slide.link,
+      idContainer
+    );
+  });
 }
